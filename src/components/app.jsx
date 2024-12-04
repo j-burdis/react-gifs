4a8ce04823102d4bd6ef8aa0f0b4705d2599e5ae
Jonathan Burdis <jonnyburdis@hotmail.com>
2024-12-04 17:40:35 +0000

    add gif search function

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
+import giphy from 'giphy-api';
+
 import SearchBar from './search_bar';
 import Gif from './gif';
+import GifList from './gif_list';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "KwzjWfME1fNxC"
+    }
+  }
+
+  search = (query) => {
+    giphy('Gtes86zn9Kv90ycc1RHX3cCj0hsa7JYt').search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (error, result) => {
+      this.setState({
+        gifs: result.data
+      });
+    });
+  }
+
   render() {
+    // const gifs = [
+    //   {id: "KwzjWfME1fNxC" },
+    //   {id: "yOZ5hsdLjAp8Y" }
+    // ];
+
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchFunction={this.search} />
           <div className="selected-gif">
-            <Gif />
+            <Gif id={this.state.selectedGifId} />
           </div>
         </div>
-        <div className="right-scene"></div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} />
+        </div>
       </div>
     );
   }
--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 
 class Gif extends Component {
   render() {
-    const src = "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExa2syenU5c2R4emJyZzR5d2VuMHp3N2M4ZDBjOHZsNWZveTkzbXFzayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/KwzjWfME1fNxC/giphy.gif"
+    const src = `https://media2.giphy.com/media/${this.props.id}/200.gif`;
     return (
-      <img src={src} alt="Seal scratch" className="gif"/>
+      <img src={src} alt="" className="gif"/>
     );
   }
 }
